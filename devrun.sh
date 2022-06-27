900077c15710d5ba22bffd84ed01abb99de3122a
Felipe Quintella <felipe@quintella.com>
2022-06-27 20:11:46 -0300

    upgrade to 7.17

--- devrun.sh
+++ devrun.sh
@@ -1,1 +1,1 @@
-docker run  -p 80:8080 -p 4440:4440 --name puppet --rm -ti ffquintella/docker-puppet:latest /bin/bash
+docker run  -p 8080:8080 -p 4440:4440 --name puppet --rm -ti ffquintella/docker-puppet:latest /bin/bash
